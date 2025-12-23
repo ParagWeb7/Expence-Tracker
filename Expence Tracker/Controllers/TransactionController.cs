dc556a6ff37442ce21b43b7e3c846b888022be55
ParagWeb7 <paragweb7@gmail.com>
2025-12-23 14:40:19 +0530

    Add project files.

---
+++ Expence Tracker/Controllers/TransactionController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Expence_Tracker.Models;
+
+
+namespace Expence_Tracker.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Transactions.Include(t => t.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+
+        // GET: Transaction/AddorEdit
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            PopulateCategories();
+
+            if (id == 0)
+                return View(new Transaction());
+            else
+            {
+                var transaction = _context.Transactions
+                    .Include(t => t.Category)
+                    .FirstOrDefault(t => t.TransactionId == id);
+                return View(transaction);
+            }
+        }
+
+
+        private void PopulateCategories()
+        {
+            var categories = _context.Categories
+                .Select(c => new
+                {
+                    c.CategoryId,
+                    TitleWithIcon = c.Icon + " " + c.Title
+                }).ToList();
+
+            ViewBag.CategoryId = new SelectList(categories, "CategoryId", "TitleWithIcon");
+        }
+
+
+
+        // POST: Transaction/AddorEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,CategoryId,Amount,Note,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if (transaction.TransactionId == 0)
+                    _context.Add(transaction);
+                else
+                    _context.Update(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            PopulateCategories();
+            return View(transaction);
+        }
+
+
+        // GET: Transaction/AddOrEdit
+
+
+        // POST: Transaction/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TransactionId,Amount,Note,Date")] Transaction transaction)
+        {
+            if (id != transaction.TransactionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(transaction);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransactionExists(transaction.TransactionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transaction);
+        }
+
+
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transactions == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Transactions'  is null.");
+            }
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool TransactionExists(int id)
+        {
+            return (_context.Transactions?.Any(e => e.TransactionId == id)).GetValueOrDefault();
+        }
+        [NonAction]
+        public void LoadCategoryDropdown()
+        {
+            var CategoryCollection = _context.Categories.ToList();
+            Category DefaultCategory = new Category() { CategoryId = 0, Title = "Choose a Category" };
+            CategoryCollection.Insert(0, DefaultCategory);
+            ViewBag.CategoryId = new SelectList(CategoryCollection, "CategoryId", "Title");
+        }
+
+    }
+}
